4a1bfaf2f70fbdf9b91e1e03dda85e753f262be7
Yuan Bozhi <lawby1229@163.com>
2013-07-16 22:14:17 +0800

    增加audio背景变化的接口

---
+++ FM916Web/src/com/fm916web/managers/IAudioProgram.java
@@ -0,0 +1,7 @@
+package com.fm916web.managers;
+
+import java.io.File;
+
+public interface IAudioProgram {
+	public boolean UploadBackground(File file, String fileName);
+}
---
+++ FM916Web/src/com/fm916web/managers/impl/AudioProgramImp.java
@@ -0,0 +1,43 @@
+package com.fm916web.managers.impl;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.struts2.ServletActionContext;
+
+import com.fm916web.managers.IAudioProgram;
+import com.opensymphony.xwork2.ActionContext;
+
+public class AudioProgramImp implements IAudioProgram {
+
+	public boolean UploadBackground(File file, String fileName) {
+		try {
+			ServletContext sc = ServletActionContext.getServletContext();
+			FileInputStream fis = new FileInputStream(file);
+			FileOutputStream fos;
+			fos = new FileOutputStream(new File(sc.getRealPath("/")
+					.concat("photo/").concat("audiobackground/")));
+			byte[] b = new byte[1];
+			while (fis.read(b) != -1) {
+				fos.write(b);
+				fos.flush();
+			}
+			fis.close();
+			fos.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+}
